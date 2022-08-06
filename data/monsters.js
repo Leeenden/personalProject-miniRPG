549c95cb6e025c7532e7c9f59b10e54507c720a7
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-08-06 22:55:22 +0100

    randomised attacks

--- data/monsters.js
+++ data/monsters.js
@@ -36,6 +36,6 @@ const monsters = {
         animate: true,
         isEnemy:  true,
         name: "Draggle",
-        attacks: [attacks.Tackle]
+        attacks: [attacks.Tackle, attacks.Fireball]
     }
 }
