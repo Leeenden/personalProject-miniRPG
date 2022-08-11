52aae1694f934811f37bdd59c662ac01157c7b14
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-08-11 20:27:53 +0100

    re-push

--- overworld.js
+++ overworld.js
@@ -26,9 +26,6 @@ const skillCard6 = document.querySelector("#sc6");
 skillCard6.innerHTML = skills.Six.name;
 
 
-// const skillCard4 = document.querySelector("#sc4");
-// const skillCard5 = document.querySelector("#sc5");
-// const skillCard6 = document.querySelector("#sc6");
 // // const skillCard7 = document.querySelector("#sc7");
 // // const skillCard8 = document.querySelector("#sc8");
 // // const skillCard9 = document.querySelector("#sc9");
@@ -36,12 +33,6 @@ skillCard6.innerHTML = skills.Six.name;
 // // const skillCard11 = document.querySelector("#sc11");
 // // const skillCard12= document.querySelector("#sc12");
 
-
-// skillCard2.innerHTML = skills.Two.name;
-// skillCard3.innerHTML = skills.Three.name;
-// skillCard4.innerHTML = skills.Four.name;
-// skillCard5.innerHTML = skills.Five.name;
-// skillCard6.innerHTML = skills.Six.name;
 // // skillCard7.innerHTML = skills.Seven.name;
 
 
